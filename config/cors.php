222f29d33ce5e8b47435bbdeb234aebe5641918e
Junaid Mazhar <mzrjunaid@gmail.com>
2026-03-23 00:38:12 +0500

    trying

--- config/cors.php
+++ config/cors.php
@@ -24,6 +24,10 @@ return [
         'http://[::1]:5173',
         'http://127.0.0.1:8000',
         'http://localhost:8000',
+        'https://pakquiz.com',
+        'https://www.pakquiz.com',
+        'http://pakquiz.com',
+        'http://www.pakquiz.com',
     ],
 
     'allowed_origins_patterns' => [],
