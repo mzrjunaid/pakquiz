87c7db0ad014de1d02f9eaff5ffcbd32392de9a8
Junaid <mzrjunaid@gmail.com>
2026-04-08 17:07:05 +0500

    new update with demo page to livewire

--- app/Http/Controllers/Admin/actions/App/Http/Controllers/Public/index.ts
+++ app/Http/Controllers/Admin/actions/App/Http/Controllers/Public/index.ts
@@ -2,13 +2,11 @@ import AdminMcqImportController from './AdminMcqImportController'
 import SearchController from './SearchController'
 import SubjectController from './SubjectController'
 import PremiumController from './PremiumController'
-import HomeController from './HomeController'
 const Public = {
     AdminMcqImportController: Object.assign(AdminMcqImportController, AdminMcqImportController),
 SearchController: Object.assign(SearchController, SearchController),
 SubjectController: Object.assign(SubjectController, SubjectController),
 PremiumController: Object.assign(PremiumController, PremiumController),
-HomeController: Object.assign(HomeController, HomeController),
 }
 
 export default Public
---
+++ public/build/assets/chevron-down-BmULIA-C.js
@@ -0,0 +1,1 @@
+import{c as o}from"./createLucideIcon-Qn4rEa0T.js";const n=[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]],c=o("ChevronDown",n);export{c as C};
