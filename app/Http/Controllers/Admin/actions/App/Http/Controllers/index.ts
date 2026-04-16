d3da961ed778920b3030f50ab213f15fc655ac2c
Junaid Mazhar <mzrjunaid@gmail.com>
2026-04-16 09:20:57 +0500

    update

--- app/Http/Controllers/Admin/actions/App/Http/Controllers/index.ts
+++ app/Http/Controllers/Admin/actions/App/Http/Controllers/index.ts
@@ -1,11 +1,9 @@
 import Admin from './Admin'
 import Public from './Public'
-import ChatController from './ChatController'
 import Settings from './Settings'
 const Controllers = {
     Admin: Object.assign(Admin, Admin),
 Public: Object.assign(Public, Public),
-ChatController: Object.assign(ChatController, ChatController),
 Settings: Object.assign(Settings, Settings),
 }
 
